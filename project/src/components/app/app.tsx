ecebb572083f3bec94244918fc11dae360a4ef9b
koteech <nosenkoaa@mbsi.bz>
2022-07-22 00:21:49 +0300

    Revert "2.9. Начинаем программировать"
    This reverts commit 0ebbf8f385c77bb544ca5076e895958f6fda64d8.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,19 +1,5 @@
-import MainPage from '../../pages/main-page/main-page';
-
-type AppPageProps = {
-  filmCard: {
-    title: string,
-    genre: string,
-    year: number
-  }
-};
-
-function App({filmCard}: AppPageProps): JSX.Element {
-  return (
-    <MainPage
-      filmCard = {filmCard}
-    />
-  );
+function App(): JSX.Element {
+  return <p>Hello, world!</p>;
 }
 
 export default App;
