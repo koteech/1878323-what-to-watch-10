a7234cc1434eb4eb4d5a3b5079c8d71e7ddea58d
koteech <nosenkoaa@mbsi.bz>
2022-09-03 14:35:42 +0300

    Доработка по замечаниям случайного наставника

--- project/src/store/app-process/app-process.test.ts
+++ project/src/store/app-process/app-process.test.ts
@@ -23,46 +23,46 @@ describe('Reducer: app', () => {
 
   it('should change Genre, update genre to payload and set showCount to SHOW_FILM_COUNT', () => {
     expect(appProcess.reducer(state, changeGenre(Genre['All genres'])))
-      .toEqual({genre: Genre['All genres'], loadingObject: '', showCount: SHOW_FILM_COUNT});
+      .toEqual({genre: Genre['All genres'], loadingObject: '', showCount: SHOW_FILM_COUNT, isLoadError: false});
   });
 
   it('should increase showCounter for showCounter + SHOW_FILM_COUNT', () => {
     expect(appProcess.reducer(state, showMore()))
-      .toEqual({genre: Genre['All genres'], loadingObject: '', showCount: SHOW_FILM_COUNT * 2});
+      .toEqual({genre: Genre['All genres'], loadingObject: '', showCount: SHOW_FILM_COUNT * 2, isLoadError: false});
   });
 
   it('should reset film count, set showCount to SHOW_FILM_COUNT', () => {
     expect(appProcess.reducer(state, resetFilmCount()))
-      .toEqual({genre: Genre['All genres'], loadingObject: '', showCount: SHOW_FILM_COUNT});
+      .toEqual({genre: Genre['All genres'], loadingObject: '', showCount: SHOW_FILM_COUNT, isLoadError: false});
   });
 
   describe('fetchFilmsAction test', () => {
     it('should set loadingObject to LoadingObject.Films if fetchFilmsAction pending', () => {
       expect(appProcess.reducer(state, {type: fetchFilmsAction.pending.type}))
-        .toEqual({genre: Genre['All genres'], loadingObject: LoadingObject.Films, showCount: SHOW_FILM_COUNT});
+        .toEqual({genre: Genre['All genres'], loadingObject: LoadingObject.Films, showCount: SHOW_FILM_COUNT, isLoadError: false});
     });
     it('should set loadingObject to \'\' if fetchFilmsAction fulfilled', () => {
       expect(appProcess.reducer(state, {type: fetchFilmsAction.fulfilled.type}))
-        .toEqual({genre: Genre['All genres'], loadingObject: '', showCount: SHOW_FILM_COUNT});
+        .toEqual({genre: Genre['All genres'], loadingObject: '', showCount: SHOW_FILM_COUNT, isLoadError: false});
     });
     it('should set loadingObject to \'\' if fetchFilmsAction rejected', () => {
       expect(appProcess.reducer(state, {type: fetchFilmsAction.rejected.type}))
-        .toEqual({genre: Genre['All genres'], loadingObject: '', showCount: SHOW_FILM_COUNT});
+        .toEqual({genre: Genre['All genres'], loadingObject: '', showCount: SHOW_FILM_COUNT, isLoadError: false});
     });
   });
 
   describe('fetchFilmAction test', () => {
     it('should set loadingObject to LoadingObject.Film if fetchFilmAction pending', () => {
       expect(appProcess.reducer(state, {type: fetchFilmAction.pending.type}))
-        .toEqual({genre: Genre['All genres'], loadingObject: LoadingObject.Film, showCount: SHOW_FILM_COUNT});
+        .toEqual({genre: Genre['All genres'], loadingObject: LoadingObject.Film, showCount: SHOW_FILM_COUNT, isLoadError: false});
     });
     it('should set loadingObject to \'\' if fetchFilmAction fulfilled', () => {
       expect(appProcess.reducer(state, {type: fetchFilmAction.fulfilled.type}))
-        .toEqual({genre: Genre['All genres'], loadingObject: '', showCount: SHOW_FILM_COUNT});
+        .toEqual({genre: Genre['All genres'], loadingObject: '', showCount: SHOW_FILM_COUNT, isLoadError: false});
     });
     it('should set loadingObject to \'\' if fetchFilmAction rejected', () => {
       expect(appProcess.reducer(state, {type: fetchFilmAction.rejected.type}))
-        .toEqual({genre: Genre['All genres'], loadingObject: '', showCount: SHOW_FILM_COUNT});
+        .toEqual({genre: Genre['All genres'], loadingObject: '', showCount: SHOW_FILM_COUNT, isLoadError: false});
     });
   });
 });
